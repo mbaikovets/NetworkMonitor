b20760a348f3a4b60abba66d3eb1fa75536f67c5
Maksym Baikovets <maksym.baikovets@pm.bet>
2022-04-03 16:00:12 +0300

    patch: AsyncMonitor and NetworkMonitor

---
+++ Sources/NetworkMonitor/AsyncMonitor.swift
@@ -0,0 +1,23 @@
+import Network
+
+@available(iOS 13.0, *)
+public struct AsyncMonitor {
+    static private let queue = DispatchQueue.global(qos: .utility)
+    
+    static public let isNetworkReachable: AsyncStream<Bool> = AsyncStream { continuation in
+        let monitor = NWPathMonitor()
+        
+        monitor.pathUpdateHandler = { path in
+            let isSatisfied = path.status == .satisfied
+                && (!path.usesInterfaceType(.other) || !path.usesInterfaceType(.loopback))
+            
+            continuation.yield(isSatisfied)
+        }
+        
+        continuation.onTermination = { @Sendable _ in
+            monitor.cancel()
+        }
+        
+        monitor.start(queue: queue)
+    }
+}
--- Sources/NetworkMonitor/NetworkMonitor.swift
+++ Sources/NetworkMonitor/NetworkMonitor.swift
@@ -1,6 +1,29 @@
-public struct NetworkMonitor {
-    public private(set) var text = "Hello, World!"
+import Network
 
-    public init() {
+public typealias ConnectionStatus = NWPath.Status
+
+public class NetworkMonitor {
+    static public let shared = NetworkMonitor()
+    
+    public private(set) var status: ConnectionStatus = .unsatisfied
+    public private(set) var isNetworkReachable: Bool = false
+    
+    private let monitor: NWPathMonitor
+    private let queue = DispatchQueue.global(qos: .utility)
+    
+    private init() {
+        self.monitor = NWPathMonitor()
+        self.monitor.start(queue: queue)
+    }
+    
+    public func startMonitoring() {
+        self.monitor.pathUpdateHandler = { path in
+            self.status = path.status
+            
+            let isSatisfied = path.status == .satisfied
+            let isValidPath = !path.usesInterfaceType(.other) || !path.usesInterfaceType(.loopback)
+            
+            self.isNetworkReachable = isSatisfied && isValidPath
+        }
     }
 }
